e44e12ba10d568c471c6271c74760cb2b823ab0c
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-29 00:55:26 +0300

    Update RevisingTheSelectQueryII.sql

--- RevisingTheSelectQueryII.sql
+++ RevisingTheSelectQueryII.sql
@@ -1,7 +1,4 @@
-###**[Revising the Select Query-1](https://www.hackerrank.com/challenges/revising-the-select-query)**
-
---Revising the Select Query II
---https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+--**[Revising the Select Query II](https://www.hackerrank.com/challenges/revising-the-select-query-2)**
 
 ###**[Revising the Select Query-2](https://www.hackerrank.com/challenges/revising-the-select-query)**
       
