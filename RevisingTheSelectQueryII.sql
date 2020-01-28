8bc7c77e9d0adb8445b43de160194aede4c7aedb
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-29 00:48:33 +0300

    Update RevisingTheSelectQueryII.sql

--- RevisingTheSelectQueryII.sql
+++ RevisingTheSelectQueryII.sql
@@ -1,6 +1,8 @@
 --Revising the Select Query II
 --https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
 
+###**[Revising the Select Query II](https://www.hackerrank.com/challenges/weather-observation-station-19/problem?h_r=next-challenge&h_v=legacy)**
+      
 select name
 from city
 where countrycode = 'USA'
