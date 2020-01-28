8fab179f4e6cf483e9f48686d63d9444f25fe8e0
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-29 00:50:47 +0300

    Update RevisingTheSelectQueryII.sql

--- RevisingTheSelectQueryII.sql
+++ RevisingTheSelectQueryII.sql
@@ -1,7 +1,7 @@
 --Revising the Select Query II
 --https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
 
-###**[Revising the Select Query II](https://www.hackerrank.com/challenges/revising-the-select-query-2/problem)**
+###**[Revising the Select Query II](https://www.hackerrank.com/challenges/revising-the-select-query-2)**
       
 select name
 from city
