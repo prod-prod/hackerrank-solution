33a67df3f24e45b2ac02fd8cfedee872d61c0220
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-29 00:59:16 +0300

    Update RevisingTheSelectQueryII.sql

--- RevisingTheSelectQueryII.sql
+++ RevisingTheSelectQueryII.sql
@@ -2,9 +2,9 @@
 --https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
 
 
-```sql      
+      
 select name
 from city
 where countrycode = 'USA'
 and population > 120000;
-```
+
