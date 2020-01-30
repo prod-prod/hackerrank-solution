8cb7f13bdfd47d838b5034db49db41f72f412b5e
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-30 19:12:43 +0300

    Delete RevisingTheSelectQueryII.sql

--- RevisingTheSelectQueryII.sql
+++
@@ -1,9 +0,0 @@
---Revising the Select Query II
---https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
-
-
-select name
-from city
-where countrycode = 'USA'
-and population > 120000;
-
