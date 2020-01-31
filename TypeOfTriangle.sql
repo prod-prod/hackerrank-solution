74c14feb401fa5670ca2fcc0737d7f65a5af0bee
prod-prod <58140024+prod-prod@users.noreply.github.com>
2020-01-31 11:50:37 +0300

    Create TypeOfTriangle.sql

---
+++ TypeOfTriangle.sql
@@ -0,0 +1,13 @@
+--Type of Triangle
+--https://www.hackerrank.com/challenges/what-type-of-triangle
+
+select 
+  case 
+  when A+B <= C or A+C <= B or B+C <= A then 'Not A Triangle'
+  when A=B and B=C then 'Equilateral'
+  when A=B or A=C  or B=C  then 'Isosceles'  
+  when A<>B and B<>C then 'Scalene' 
+
+  end as triangles_type 
+  
+from TRIANGLES; 
